537454ab772c2fa222bea5d3fc20237c234cedc8
kevin-ngn <apacheservices68@gmail.com>
2015-12-28 13:52:16 +0700

    more changes to index

--- resources/views/public/home/create.blade.php
+++ resources/views/public/home/create.blade.php
@@ -1,28 +1,7 @@
 <div class="container-fluid">
 	{!! Form::open(array('url' => URL::to('item'), 'method' => 'post', 'class' => 'form-horizontal', 'files'=> false,'id'=>'form-item')) !!}
 		<div class="row">
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				@if (count($errors) > 0)  
-			        <div class="alert alert-danger alert-dismissible" role="alert">
-			          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			          <ol>
-			          {!! implode('', $errors->all('<li class="error">:message</li>')) !!}
-			          </ol>          
-			        </div>
-				@endif	
-				@if(Session::has('alert_success'))
-		          <div class="alert alert-success alert-dismissible" role="alert">
-		            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-		            {{ Session::get("alert_success") }}		            
-		          </div>
-		        @endif
-		        @if(Session::has('alert_danger'))
-		          <div class="alert alert-success alert-dismissible" role="alert">
-		            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-		            {{ Session::get("alert_danger") }}		            
-		          </div>
-		        @endif
-			</div>
+			@include('public.home.status')
 			<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
 				<div class="panel panel-default">			
 					<div class="panel-body">			   
